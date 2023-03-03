6c8cfdba48a2051ed34bf40bc4c17a5eaaba872e
YOUNGmaxer <517538470@qq.com>
2023-03-03 23:32:29 +0800

    fix: 修复单测跑不通

--- test/server/file-server-helper.test.ts
+++ test/server/file-server-helper.test.ts
@@ -2,7 +2,11 @@ import { readDirectory } from '@/server/file-server-helper'
 import { expect, it } from 'vitest'
 
 it('readDirectory', async () => {
-  const path = '/Users/admin/Downloads/'
+  const path = '/Users/azeryang/Downloads'
   const files = await readDirectory(path)
-  expect(files).toBe([])
+  expect(files).toEqual([
+    '.DS_Store',
+    '.localized',
+    'fig.dmg'
+  ])
 })
